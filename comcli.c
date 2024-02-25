f76abd2e776ab19db6f076cb20e910977734922b
gorkemsolun <gorkemkadir14@hotmail.com>
2024-02-25 15:48:05 +0300

    Execute single command lines

--- comcli.c
+++ comcli.c
@@ -24,70 +24,54 @@ int main(int argc, char* argv[]) {
      strcat(mq_name, argv[1]);
 
      mq = mq_open(mq_name, O_RDWR);
-     if (mq == -1) {
-          perror("mq_open");
-          exit(1);
-     }
 
      strcpy(buffer, "Let me connect");
-     if (mq_send(mq, buffer, strlen(buffer), 0) == -1) {
-          perror("mq_send");
-          exit(1);
-     }
+     mq_send(mq, buffer, strlen(buffer), 0);
 
-     while (strcmp(buffer, "You are connected") != 0) {
-          if (mq_receive(mq, buffer, 1024, NULL) == -1) {
-               perror("mq_receive");
-               exit(1);
-          }
-          sleep(1);
-     }
+     mq_receive(mq, buffer, 1024, NULL);
+
      printf("Received success of connection: %s\n", buffer);
 
+     //creation of pipes
      pid_t pid = getpid();
      char csPipeName[256], scPipeName[256];
      sprintf(csPipeName, "cs%d", pid);
      sprintf(scPipeName, "sc%d", pid);
      int cs = mkfifo(csPipeName, 0666);
-     if (cs == -1) {
-          perror("Error creating cs pipe\n");
-     }
      int sc = mkfifo(scPipeName, 0666);
-     if (sc == -1) {
-          perror("Error creating sc pipe\n");
-     }
+
 
      sprintf(buffer, "%s %d %s %s 1024", mq_name, pid, csPipeName, scPipeName);
-     if (mq_send(mq, buffer, strlen(buffer), 0) == -1) {
-          perror("mq_send");
-          exit(1);
-     }
+     mq_send(mq, buffer, strlen(buffer), 0);
+
+     sc = open(scPipeName, O_WRONLY);
+     cs = open(csPipeName, O_RDONLY);
 
      while (1) {
-          // here should send the commands to cs pipe
-          printf("Please give a line of command to execute: \n");
-          fgets(buffer, 1024, stdin);
-          printf("%s", buffer);
-
-          sc = open(scPipeName, O_WRONLY);
-          if (sc == -1) {
-               perror("Error opening sc pipe for writing");
-          }
-          write(sc, buffer, 1024);
-
-          cs = open(csPipeName, O_RDONLY);
-          if (cs == -1) {
-               perror("Error opening cs pipe for writing");
-          }
           readSize = read(cs, buffer, 1024);
           buffer[readSize] = '\0';
-          printf("received over cs pipe: %s\n", buffer);
+          printf("%s\n", buffer);
 
-          if (mq_close(mq) == -1) {
-               perror("mq_close");
-               exit(1);
-          }
-          return 0;
+          printf("Please give a line of command to execute: \n");
+          fgets(buffer, 1024, stdin);
+          write(sc, buffer, 1024);
+
+          readSize = read(cs, buffer, 1024);
+          buffer[readSize] = '\0';
+          printf("%s\n", buffer);
+
+
+          break;
      }
 
+     close(sc);
+     close(cs);
+     unlink(scPipeName);
+     unlink(csPipeName);
+
+     if (mq_close(mq) == -1) {
+          perror("mq_close");
+          exit(1);
+     }
+     return 0;
 }
--- comserver.c
+++ comserver.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <mqueue.h>
 #include <stdio.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -14,58 +15,35 @@ int main(int argc, char* argv[]) {
           return 0;
      }
 
-     int i, readSize;
+     int i, readSize, wsize, cs, sc, file, rd, wr;
      mqd_t mq;
+     pid_t n, nn, clientID;
      struct mq_attr attr;
-     char buffer[1024];
+     char mq_name[256], csPipeName[256], scPipeName[256], buffer[1024], * token, * args[] = { NULL }, fileName[256];
 
      attr.mq_flags = 0;
      attr.mq_maxmsg = 10;
      attr.mq_msgsize = 1024;
      attr.mq_curmsgs = 0;
 
-     char mq_name[256];
      strcpy(mq_name, "/");
      strcat(mq_name, argv[1]);
 
      mq = mq_open(mq_name, O_CREAT | O_RDWR, 0666, &attr);
-     if (mq == -1) {
-          perror("mq_open");
-          exit(1);
-     }
 
      while (1) {
-
-          while (strcmp(buffer, "Let me connect") != 0) {
-               if (mq_receive(mq, buffer, 1024, NULL) == -1) {
-                    perror("mq_receive");
-                    exit(1);
-               }
-               sleep(1);
-          }
+          mq_receive(mq, buffer, 1024, NULL);
           printf("Received connection: %s\n", buffer);
 
           strcpy(buffer, "You are connected");
-          if (mq_send(mq, buffer, strlen(buffer), 0) == -1) {
-               perror("mq_send");
-               exit(1);
-          }
+          mq_send(mq, buffer, strlen(buffer), 0);
 
-          while (strncmp(buffer, mq_name, strlen(mq_name)) != 0) {
-               if (mq_receive(mq, buffer, 1024, NULL) == -1) {
-                    perror("mq_receive");
-                    exit(1);
-               }
-          }
+          mq_receive(mq, buffer, 1024, NULL);
           printf("Received cs, sc pipes: %s\n", buffer);
 
-          //parse the message, ID of client, name of the pipes created by client, wsize
+          // parse the message, ID of client, name of the pipes created by client, wsize
 
-          char* token;
           token = strtok(buffer, " ");
-          pid_t clientID;
-          char csPipeName[256], scPipeName[256];
-          int wsize, cs, sc;
           i = 0;
 
           while (token != NULL) {
@@ -80,45 +58,78 @@ int main(int argc, char* argv[]) {
                     wsize = atoi(token);
                }
                i++;
-               printf("%s\n", token);
                token = strtok(NULL, " ");
           }
 
-          // create server child with fork() TODO
+          // TODO create server child with fork()
 
-
-          pid_t n;
           n = fork();
 
-          if (n < 0) {
-               fprintf(stderr, "fork Failed");
-               exit(-1);
-          } else if (n == 0) {
-               // TODO there may be a while loop to restrain the child
-               printf("child exec\n");
+          if (n == 0) { // child
+               sc = open(scPipeName, O_RDONLY);
+               cs = open(csPipeName, O_WRONLY);
+
+               sprintf(fileName, "%d.txt", clientID);
+               file = open(fileName, O_RDWR | O_CREAT, 0666);
 
                while (1) {
-                    sc = open(scPipeName, O_RDONLY);
-                    if (sc == -1) {
-                         perror("Error opening sc pipe for reading");
-                    }
-
-                    readSize = read(sc, buffer, 1024);
-                    buffer[readSize] = '\0';
-                    printf("received over sc pipe: %s\n", buffer);
-
-                    strcpy(buffer, "result");
-                    cs = open(csPipeName, O_WRONLY);
-                    if (cs == -1) {
-                         perror("Error opening cs pipe for writing");
-                    }
-                    
+                    strcpy(buffer, "server is connected to pipes");
                     write(cs, buffer, 1024);
 
-                    return 0;// TODO make loop end
-               }
-               return 0;
+                    readSize = read(sc, buffer, 1024); // parsing of the commands should be changed to accomadate arguments
+                    buffer[readSize] = '\0';
+                    printf("%s\n", buffer);
 
+                    i = 0;
+                    token = strtok(buffer, " ");
+                    while (token != NULL) {
+                         if (token[strlen(token) - 1] == '\n') {
+                              token[strlen(token) - 1] = '\0';
+                         }
+                         args[i++] = token;
+                         token = strtok(NULL, " ");
+                    }
+                    args[i] = NULL;
+
+                    nn = fork();
+
+                    if (nn == 0) { // grandchild
+                         dup2(file, 1); // redirect output
+                         dup2(file, 2);
+
+                         if (execvp(args[0], args) == -1) {
+                              perror("execvp");
+                              exit(1);
+                         }
+
+                    }
+                    wait(NULL);
+
+                    file = open(fileName, O_RDWR, 0666);
+                    rd = read(file, buffer, sizeof(buffer));
+                    wr = write(cs, buffer, sizeof(buffer));
+
+                    /*
+                    // Read from the file and write to the pipe
+                    while ((rd = read(file, buffer, sizeof(buffer))) > 0) {
+                         if (write(cs, buffer, sizeof(buffer)) != rd) {
+                              perror("write");
+                              exit(1);
+                         }
+                         printf("%s", buffer);
+                    }
+                    */
+
+
+                    break;
+               }
+
+               close(file);
+               close(cs);
+               close(sc);
+               unlink(scPipeName);
+               unlink(csPipeName);
+               return 0;
           }
      }
 
