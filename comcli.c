23129cf2e94ccf2e9c3097529420061b9d1e4d9e
gorkemsolun <gorkemkadir14@hotmail.com>
2024-02-25 12:01:00 +0300

    pipes initial

--- comcli.c
+++ comcli.c
@@ -1,4 +1,8 @@
 #include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/stat.h>
+#include <unistd.h>
 #include <mqueue.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,10 +12,11 @@
 int main(int argc, char* argv[]) {
      // TODO this should be changed after implementaion of sc,cs pipes
      if (/* argc != 6 && argc != 4*/ argc != 2) {
-          printf("Please provide proper amount of arguments.");
+          printf("Please provide proper amount of arguments.\n");
           return 0;
      }
 
+     int readSize, i;
      mqd_t mq;
      char buffer[1024];
      char mq_name[256];
@@ -24,40 +29,65 @@ int main(int argc, char* argv[]) {
           exit(1);
      }
 
-     strcpy(buffer, "Let me connect to the server!!");
+     strcpy(buffer, "Let me connect");
      if (mq_send(mq, buffer, strlen(buffer), 0) == -1) {
           perror("mq_send");
           exit(1);
      }
 
-     if (mq_receive(mq, buffer, 1024, NULL) == -1) {
-          perror("mq_receive");
-          exit(1);
+     while (strcmp(buffer, "You are connected") != 0) {
+          if (mq_receive(mq, buffer, 1024, NULL) == -1) {
+               perror("mq_receive");
+               exit(1);
+          }
+          sleep(1);
      }
-     printf("Received: %s\n", buffer);
+     printf("Received success of connection: %s\n", buffer);
 
-     // create cs,sc pipes, send the names, 
+     pid_t pid = getpid();
+     char csPipeName[256], scPipeName[256];
+     sprintf(csPipeName, "cs%d", pid);
+     sprintf(scPipeName, "sc%d", pid);
+     int cs = mkfifo(csPipeName, 0666);
+     if (cs == -1) {
+          perror("Error creating cs pipe\n");
+     }
+     int sc = mkfifo(scPipeName, 0666);
+     if (sc == -1) {
+          perror("Error creating sc pipe\n");
+     }
 
-     strcpy(buffer, "Sent you the sc,cs pipe names");
+     sprintf(buffer, "%s %d %s %s 1024", mq_name, pid, csPipeName, scPipeName);
      if (mq_send(mq, buffer, strlen(buffer), 0) == -1) {
           perror("mq_send");
           exit(1);
      }
 
-     if (mq_receive(mq, buffer, 1024, NULL) == -1) {
-          perror("mq_receive");
-          exit(1);
-     }
-     printf("Received: %s\n", buffer);
+     while (1) {
+          // here should send the commands to cs pipe
+          printf("Please give a line of command to execute: \n");
+          fgets(buffer, 1024, stdin);
+          printf("%s", buffer);
 
-     if (mq_close(mq) == -1) {
-          perror("mq_close");
-          exit(1);
+          sc = open(scPipeName, O_WRONLY);
+          if (sc == -1) {
+               perror("Error opening sc pipe for writing");
+          }
+          write(sc, buffer, 1024);
+
+          cs = open(csPipeName, O_RDONLY);
+          if (cs == -1) {
+               perror("Error opening cs pipe for writing");
+          }
+          readSize = read(cs, buffer, 1024);
+          buffer[readSize] = '\0';
+          printf("received over cs pipe: %s\n", buffer);
+
+          if (mq_close(mq) == -1) {
+               perror("mq_close");
+               exit(1);
+          }
+          return 0;
      }
 
-     if (mq_unlink(mq_name) == -1) {
-          perror("mq_unlink");
-          exit(1);
-     }
-     return 0;
 }
--- comserver.c
+++ comserver.c
@@ -2,6 +2,8 @@
 #include <mqueue.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <unistd.h>
 #include <string.h>
 #include "Message.h"
@@ -12,6 +14,7 @@ int main(int argc, char* argv[]) {
           return 0;
      }
 
+     int i, readSize;
      mqd_t mq;
      struct mq_attr attr;
      char buffer[1024];
@@ -32,11 +35,15 @@ int main(int argc, char* argv[]) {
      }
 
      while (1) {
-          if (mq_receive(mq, buffer, 1024, NULL) == -1) {
-               perror("mq_receive");
-               exit(1);
+
+          while (strcmp(buffer, "Let me connect") != 0) {
+               if (mq_receive(mq, buffer, 1024, NULL) == -1) {
+                    perror("mq_receive");
+                    exit(1);
+               }
+               sleep(1);
           }
-          printf("Received: %s\n", buffer);
+          printf("Received connection: %s\n", buffer);
 
           strcpy(buffer, "You are connected");
           if (mq_send(mq, buffer, strlen(buffer), 0) == -1) {
@@ -44,25 +51,77 @@ int main(int argc, char* argv[]) {
                exit(1);
           }
 
-          if (mq_receive(mq, buffer, 1024, NULL) == -1) {
-               perror("mq_receive");
-               exit(1);
+          while (strncmp(buffer, mq_name, strlen(mq_name)) != 0) {
+               if (mq_receive(mq, buffer, 1024, NULL) == -1) {
+                    perror("mq_receive");
+                    exit(1);
+               }
           }
-          printf("Received: %s\n", buffer);
+          printf("Received cs, sc pipes: %s\n", buffer);
 
           //parse the message, ID of client, name of the pipes created by client, wsize
 
-          // this will be sent by child actually TODO
-          strcpy(buffer, "got the sc,cs pipes, please send some commands");
-          if (mq_send(mq, buffer, strlen(buffer), 0) == -1) {
-               perror("mq_send");
-               exit(1);
+          char* token;
+          token = strtok(buffer, " ");
+          pid_t clientID;
+          char csPipeName[256], scPipeName[256];
+          int wsize, cs, sc;
+          i = 0;
+
+          while (token != NULL) {
+               if (i == 0) {
+               } else if (i == 1) {
+                    clientID = atoi(token);
+               } else if (i == 2) {
+                    strcpy(csPipeName, token);
+               } else if (i == 3) {
+                    strcpy(scPipeName, token);
+               } else {
+                    wsize = atoi(token);
+               }
+               i++;
+               printf("%s\n", token);
+               token = strtok(NULL, " ");
           }
 
-          // create server child with fork()
+          // create server child with fork() TODO
 
-          sleep(1);
+
+          pid_t n;
+          n = fork();
+
+          if (n < 0) {
+               fprintf(stderr, "fork Failed");
+               exit(-1);
+          } else if (n == 0) {
+               // TODO there may be a while loop to restrain the child
+               printf("child exec\n");
+
+               while (1) {
+                    sc = open(scPipeName, O_RDONLY);
+                    if (sc == -1) {
+                         perror("Error opening sc pipe for reading");
+                    }
+
+                    readSize = read(sc, buffer, 1024);
+                    buffer[readSize] = '\0';
+                    printf("received over sc pipe: %s\n", buffer);
+
+                    strcpy(buffer, "result");
+                    cs = open(csPipeName, O_WRONLY);
+                    if (cs == -1) {
+                         perror("Error opening cs pipe for writing");
+                    }
+                    
+                    write(cs, buffer, 1024);
+
+                    return 0;// TODO make loop end
+               }
+               return 0;
+
+          }
      }
+
 
      if (mq_close(mq) == -1) {
           perror("mq_close");
